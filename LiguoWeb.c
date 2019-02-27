758594d17def4a55b6c222a246aa5439f4aa7e63
jifukui <805013833@qq.com>
2019-02-27 16:44:41 +0800

    添加tcp

--- LiguoWeb.c
+++ LiguoWeb.c
@@ -189,6 +189,7 @@ uint8 GetDeviceModuleName(json_t *json,char *estr)
 {
 #define MAXLINE 80
 #define SERV_PORT 5000
+	uint8 flag=0;
 	printf("get name\n");
 	struct sockaddr_in servaddr;
     char buf[MAXLINE];
