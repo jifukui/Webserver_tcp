833cba14e112eeb5e16483fcd8fb874a7dee4a6d
jifukui <805013833@qq.com>
2019-03-05 10:29:59 +0800

    添加结构

--- LiguoWeb.c
+++ LiguoWeb.c
@@ -221,8 +221,10 @@ uint8 CommandHandle(const char *sstr,json_t *json,char *estr)
 uint32 PiPHandler(char *tx,char *rx,uint32 len)
 {
 	uint32 length;
+	uint32 txlen=sizeof(*tx)+1;
+	printf("The length of tx is %d \n",txlen);
 	lig_pip_read_bytes(sockfd,rx,len);
-	length=lig_pip_write_bytes(sockfd,tx,strlen(*tx)+1);
+	length=lig_pip_write_bytes(sockfd,tx,txlen);
 	if(length>0)
 	{
 		length=0;
