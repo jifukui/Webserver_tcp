f0ff6fd70bfe85a1b995274cc08fbd5e301d5e1a
jifukui <805013833@qq.com>
2019-03-21 12:26:01 +0800

    调试内存溢出问题

--- LiguoWeb.c
+++ LiguoWeb.c
@@ -490,11 +490,11 @@ uint8 GetPortInfo(json_t *json,json_t* cmd,char *estr)
 		{
 			copy=json_integer(i+1);
 			json_object_set(portinfo,"PortIndedx",copy);
-			json_decref(copy);
+			//json_decref(copy);
 			//json_object_set(portinfo,"PortIndedx",json_integer(i+1));
 			copy=json_deep_copy(portinfo);
 			json_array_append(portarr,copy);
-			json_decref(copy);
+			//json_decref(copy);
 		}
 		for(i=0;i<=LigPortNum;i++)
 		{
