d85d0e32d9fafffe150b7b940fd6e9ede90cbb6c
jifukui <805013833@qq.com>
2019-03-27 13:29:47 +0800

    添加文件上传

--- LiguoWeb.c
+++ LiguoWeb.c
@@ -1561,9 +1561,9 @@ uint8 Upgrade(json_t *json,json_t* cmd,char *estr)
 						PiPHandler(str,buf,sizeof(buf));
 						if(strstr(buf,"START"))
 						{
-							if(strstr(newfilename,"VS-1616DN-EM_VS-3232DN-EM_VS-6464DN-EM"))
+							if(strstr(newfilename,"VS-1616DN-EM"))
 							{
-								return 1;
+								flag=1;
 							}
 							else
 							{
