5eeae6e508ab2df9c204a3e3a3313b1b9680e885
jifukui <805013833@qq.com>
2019-04-01 11:19:18 +0800

    实验版

--- lig_pip.c
+++ lig_pip.c
@@ -102,7 +102,7 @@ int  lig_pip_read_bytes(int fd,char *buff,int bufflen)
         {
                 return -1;
         }
-        while(res==0)
+        while(res<0>)
         {
                r=read(m_lig_pip_fd[fd].rfd,buff,bufflen);
                if(r<0&& ( errno == EINTR || errno == EAGAIN ))
@@ -140,7 +140,7 @@ int lig_pip_write_bytes(int fd,char*buff,int datalen)
         }
 
         
-        while(res==0)
+        while(res<0)
         {
                r=write(m_lig_pip_fd[fd].wfd,buff,datalen);
                if(r<0&& ( errno == EINTR || errno == EAGAIN ))
