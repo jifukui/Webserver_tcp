94bc37672369922bfd8cd4c33699a6af8c8a5267
jifukui <805013833@qq.com>
2019-07-10 09:48:14 +0800

    添加测试

--- LiguoWeb.c
+++ LiguoWeb.c
@@ -379,7 +379,7 @@ uint32 PiPHandler(char *tx,char *rx,uint32 len)
 	}while(length);*/
 	//printf("The length is %d\n",length);
 	bzero(rx,len);
-	//printf("The send buf is %s",tx);
+	printf("The send buf is %s",tx);
 	length=lig_pip_write_bytes(sockfd,tx,strlen(tx)+1);
 	if(length>0)
 	{
@@ -409,7 +409,7 @@ uint32 PiPHandler(char *tx,char *rx,uint32 len)
 		//pid =getpid();
 		//printf("The child pid is %d \n",pid);
 	}
-	//printf("The recieve buf is %s",rx);
+	printf("The recieve buf is %s",rx);
 	return length;
 }
 
