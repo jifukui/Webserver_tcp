c5ba7f53e9047d7fa7886de3ac5400ce862b935f
jifukui <805013833@qq.com>
2020-05-03 11:22:36 +0800

    添加支持json的形式

--- LiguoWeb.c
+++ LiguoWeb.c
@@ -1626,9 +1626,9 @@ uint8 Upgrade(json_t *json,json_t* cmd,char *estr)
 						printf("The first return is %s",buf);
 						if(strstr(buf,"START"))
 						{
-							if(strstr(newfilename,"VS-1616DN-EM_VS-3232DN-EM_VS-6464DN-EM"))
+							if(strstr(newfilename,"VS-1616DN-EM_VS-3232DN-EM_VS-6464DN-EM")||strstr(newfilename,""))
 							{
-								printf("ctrl\n");
+								printf("ctrl or  json\n");
 								flag=1;
 							}
 							else
