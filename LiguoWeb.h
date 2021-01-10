22606da8fe8798cb31147005be4dc2dfa1dcd016
jifukui <805013833@qq.com>
2021-01-10 17:59:39 +0800

    添加安全协议

--- LiguoWeb.h
+++ LiguoWeb.h
@@ -1,8 +1,7 @@
 #ifndef _LIGUO_WEB_H_
 #define _LIGUO_WEB_H_
+#include <jansson.h>
 typedef unsigned char uint8;
-unsigned char LiguoWeb_GET_Method(const char *sstr,json_t * json,char *estr);
-unsigned char LiguoWeb_POST_Method(const unsigned char *sstr,json_t *json,char * estr);
 typedef struct 
 {
     char username[15];
@@ -14,4 +13,6 @@ typedef struct{
 }Auth_liguo;
 void writesecurityfile();
 uint8 CheckPassword(uint8 *password);
+unsigned char LiguoWeb_GET_Method(const char *sstr,json_t * json,char *estr);
+unsigned char LiguoWeb_POST_Method(const unsigned char *sstr,json_t *json,char * estr);
 #endif //_LIGUO_WEB_H_
